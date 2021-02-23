c40405ea133db867fec5adceb04042c8d3961c9f
Moudud Abu <32541410+MoududAbu@users.noreply.github.com>
2021-02-23 19:03:36 +0000

    adding components and styles for name and population

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Layout from '../components/layouts/layout'
 import styles from '../styles/Home.module.css'
 import SearchInput from '../components/searchInput/searchInput';
+import CountriesTable from '../components/countriesTable/countriesTable';
 
 export default function Home({ countries }) {
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
       <SearchInput placeholder="filter by Name, Region or Sub Region" />
+      <CountriesTable countries={countries} />
     </Layout>
   );
 }
