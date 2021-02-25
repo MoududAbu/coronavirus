bf5e4383a6994ee83ce7e1c40975c741ff179c44
Moudud Abu <moududabu2017@gmail.com>
2021-02-25 01:21:46 +0000

    prettier format

--- src/components/countriesTable/countriesTable.js
+++ src/components/countriesTable/countriesTable.js
@@ -3,87 +3,86 @@ import { KeyboardArrowDownRounded, KeyboardArrowUpRounded } from '@material-ui/i
 import styles from './CountriesTable.module.css';
 
 const orderBy = (countries, value, direction) => {
-    if (direction === 'asc') {
-        return [...countries].sort((a, b) => a[value] > b[value] ? 1 : -1);
-    }
+	if (direction === 'asc') {
+		return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
+	}
 
-    if (direction === 'desc') {
-        return [...countries].sort((a, b) => a[value] > b[value] ? -1 : 1);
-    }
+	if (direction === 'desc') {
+		return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
+	}
 
-    return countries;
-}
+	return countries;
+};
 
 const SortArrow = ({ direction }) => {
-    if (!direction) {
-        return <></>
-    }
+	if (!direction) {
+		return <></>;
+	}
 
-    if (direction === 'desc') {
-        return (
-            <div className={styles.heading_arrow}>
-                <KeyboardArrowDownRounded color="inherit"/>
-            </div>
-        );
-    } else {
-        return (
-            <div className={styles.heading_arrow}>
-                <KeyboardArrowUpRounded color="inherit"/>
-            </div>
-        );
-    }
-}
+	if (direction === 'desc') {
+		return (
+			<div className={styles.heading_arrow}>
+				<KeyboardArrowDownRounded color="inherit" />
+			</div>
+		);
+	} else {
+		return (
+			<div className={styles.heading_arrow}>
+				<KeyboardArrowUpRounded color="inherit" />
+			</div>
+		);
+	}
+};
 
 const renderRow = (orderedCountries) => {
-    return orderedCountries.map((country, index) =>
-        <div key={index} className={styles.row}>
-            <div className={styles.name}>{country.name}</div>
-            <div className={styles.population}>{country.population}</div>
-        </div>
-    )
-}
+	return orderedCountries.map((country, index) => (
+		<div key={index} className={styles.row}>
+			<div className={styles.name}>{country.name}</div>
+			<div className={styles.population}>{country.population}</div>
+		</div>
+	));
+};
 
 const CountriesTable = ({ countries }) => {
-    const [direction = 'desc', setDirection] = useState();
-    const [value, setValue] = useState();
+	const [direction = 'desc', setDirection] = useState();
+	const [value, setValue] = useState();
 
+	const orderedCountries = orderBy(countries, value, direction);
 
-    const orderedCountries = orderBy(countries, value, direction);
+	const switchDirection = () => {
+		if (!direction) {
+			setDirection('desc');
+		} else if (direction === 'desc') {
+			setDirection('asc');
+		} else if (direction === 'asc') {
+			setDirection('desc');
+		}
+	};
 
-    const switchDirection = () => {
-        if (!direction) {
-            setDirection('desc');
-        } else if (direction === 'desc') {
-            setDirection('asc');
-        } else if (direction === 'asc') {
-            setDirection('desc');
-        }
-    }
+	const setValueAndDirection = (value) => {
+		switchDirection();
+		setValue(value);
+	};
 
-    const setValueAndDirection = (value) => {
-        switchDirection();
-        setValue(value);
-    }
+	return (
+		<div>
+			<div className={styles.heading}>
+				<button className={styles.heading_name} onClick={() => setValueAndDirection('name')}>
+					<div>Name</div>
+					<SortArrow direction={direction} />
+				</button>
 
-    return (
-        <div>
-            <div className={styles.heading}>
-                <button className={styles.heading_name}
-                        onClick={() => setValueAndDirection('name')}>
-                    <div>Name</div>
-                    <SortArrow direction={direction}/>
-                </button>
-
-                <button
-                    className={styles.heading_population}
-                    onClick={() => setValueAndDirection('population')}>
-                    <div>Population</div>
-                    <SortArrow direction={direction}/>
-                </button>
-            </div>
-            {renderRow(orderedCountries)}
-        </div>
-    );
-}
+				<button
+					className={styles.heading_population}
+					onClick={() => setValueAndDirection('population')}
+				>
+					<div>Population</div>
+					<SortArrow direction={direction} />
+				</button>
+			</div>
+			{renderRow(orderedCountries)}
+		</div>
+	);
+};
 
 export default CountriesTable;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,22 @@ import SearchInput from '../components/searchInput/searchInput';
 import CountriesTable from '../components/countriesTable/countriesTable';
 
 export default function Home({ countries }) {
-    return (
-        <Layout>
-            <div className={styles.counts}>Found {countries.length} countries</div>
-            <SearchInput placeholder="filter by Name, Region or Sub Region"/>
-            <CountriesTable countries={countries}/>
-        </Layout>
-    );
+	return (
+		<Layout>
+			<div className={styles.counts}>Found {countries.length} countries</div>
+			<SearchInput placeholder="filter by Name, Region or Sub Region" />
+			<CountriesTable countries={countries} />
+		</Layout>
+	);
 }
 
 export const getStaticProps = async () => {
-    const res = await fetch('https://restcountries.eu/rest/v2/all');
-    const countries = await res.json();
+	const res = await fetch('https://restcountries.eu/rest/v2/all');
+	const countries = await res.json();
 
-    return {
-        props: {
-            countries
-        }
-    }
-}
+	return {
+		props: {
+			countries,
+		},
+	};
+};
