1af80a08fd0b9c9982ea557b16be7a755273c657
Moudud Abu <moududabu2017@gmail.com>
2021-03-01 04:32:20 +0000

    format

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -77,12 +77,12 @@ const Country = ({ country }) => {
 						<div className={styles.details_panel_borders_container}>
 							{borderCountries.map(({ flag, name }) => (
 								<div className={styles.details_panel_borders_country}>
-									<img src={flag} alt={name}></img> 
+									<img src={flag} alt={name}></img>
 									<div className={styles.details_panel_borders_name}>{name}</div>
 								</div>
 							))}
 						</div>
-					</div> 
+					</div>
 				</div>
 			</div>
 		</Layout>
