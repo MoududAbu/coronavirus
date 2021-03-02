f7c5362fd4034b5d572ffb9e64a3e8eab3566fed
Moudud Abu <32541410+MoududAbu@users.noreply.github.com>
2021-03-02 17:17:17 +0000

    updated endpoint for covid data

--- src/components/countriesTable/countriesTable.js
+++ src/components/countriesTable/countriesTable.js
@@ -3,6 +3,10 @@ import { KeyboardArrowDownRounded, KeyboardArrowUpRounded } from '@material-ui/i
 import styles from './countriesTable.module.css';
 import Link from 'next/link';
 
+const formatNumber = (number) => {
+	return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
+
 const orderBy = (countries, value, direction) => {
 	if (direction === 'asc') {
 		return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
@@ -37,23 +41,18 @@ const SortArrow = ({ direction }) => {
 
 const renderRow = (orderedCountries) => {
 	return orderedCountries.map((country, index) => (
-		<Link key={index} href={`/country/${country.alpha3Code}`}>
-			<div className={styles.row}>
-				<div className={styles.flag}>
-					<img src={country.flag} alt={country.name} />
-				</div>
-				<div className={styles.name}>{country.name}</div>
-				<div className={styles.population}>{country.population}</div>
-				<div className={styles.area}>{country.area || 0}</div>
-				<div className={styles.gini}>{country.gini || 0}%</div>
-			</div>
-		</Link>
+		// <Link key={index} href={`/country/${country.alpha3Code}`}>
+		<div className={styles.row}>
+			<div className={styles.name}>{country.name}</div>
+			<div className={styles.population}>{formatNumber(country.cases)}</div>
+		</div>
+		// </Link>
 	));
 };
 
 const CountriesTable = ({ countries }) => {
 	const [direction = 'desc', setDirection] = useState();
-	const [value, setValue] = useState();
+	const [value = 'cases', setValue] = useState();
 
 	const orderedCountries = orderBy(countries, value, direction);
 
@@ -81,24 +80,9 @@ const CountriesTable = ({ countries }) => {
 					{value === 'name' && <SortArrow direction={direction} />}
 				</button>
 
-				<button
-					className={styles.heading_population}
-					onClick={() => setValueAndDirection('population')}
-				>
-					<div>Population</div>
-					{value === 'population' && <SortArrow direction={direction} />}
-				</button>
-
-				<button className={styles.heading_area} onClick={() => setValueAndDirection('area')}>
-					<div>
-						Area (km<sup style={{ fontSize: '0.5rem' }}>2</sup>)
-					</div>
-					{value === 'area' && <SortArrow direction={direction} />}
-				</button>
-
-				<button className={styles.heading_gini} onClick={() => setValueAndDirection('gini')}>
-					<div>Gini</div>
-					{value === 'gini' && <SortArrow direction={direction} />}
+				<button className={styles.heading_cases} onClick={() => setValueAndDirection('cases')}>
+					<div>Cases</div>
+					{value === 'cases' && <SortArrow direction={direction} />}
 				</button>
 			</div>
 			{renderRow(orderedCountries)}
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,8 @@ import { useState } from 'react';
 export default function Home({ countries }) {
 	const [keyword, setKeyword] = useState('');
 
-	const filteredCountries = countries.filter(
-		(country) =>
-			country.name.toLowerCase().includes(keyword) ||
-			country.region.toLowerCase().includes(keyword) ||
-			country.subregion.toLowerCase().includes(keyword)
+	const filteredCountries = countries.filter((country) =>
+		country.name.toLowerCase().includes(keyword)
 	);
 
 	const onInputChange = (e) => {
@@ -25,10 +22,7 @@ export default function Home({ countries }) {
 			<div className={styles.inputContainer}>
 				<div className={styles.counts}>Found {countries.length} countries</div>
 				<div className={styles.input}>
-					<SearchInput
-						placeholder="filter by Name, Region or Sub Region"
-						onChange={onInputChange}
-					/>
+					<SearchInput placeholder="filter by Name" onChange={onInputChange} />
 				</div>
 			</div>
 			<CountriesTable countries={filteredCountries} />
@@ -37,7 +31,7 @@ export default function Home({ countries }) {
 }
 
 export const getStaticProps = async () => {
-	const res = await fetch('https://restcountries.eu/rest/v2/all');
+	const res = await fetch('https://covid-19-node.herokuapp.com/countries');
 	const countries = await res.json();
 
 	return {
